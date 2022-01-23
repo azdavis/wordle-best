805b96f5f23fc982a6dc636ec55963e661898fa5
Ariel Davis <ariel.z.davis@icloud.com>
2022-01-23 15:01:21 -0800

    Start

---
+++ readme.md
@@ -0,0 +1,34 @@
+# wordle-best
+
+An attempt to find the best starting [Wordle][1] guess.
+
+## The answer
+
+TODO (it's running right now LMAO)
+
+## Methodology
+
+A guess G is "better" than a guess G' if, given all possible answer words A,
+there are fewer words that might be the answer given the information gained from
+the guess G on the answer word A than G'.
+
+So, we find the best guess by naively O(n^3) looping over all guess words and
+all answer words, then, for each pair, finding the number of words that might
+possibly be the answer given this guess-answer pair.
+
+Then we sum over all the the answer words for each guess and return the answer
+word with the minimum overall sum.
+
+## Running the code
+
+Clone the repo and download a dictionary of words (one word per line) to
+`src/words_alpha.txt`. I got one from [here][2]. (I ignored it in the git repo
+since the file size is large.) The program automatically filters for length-5
+words.
+
+On my machine it runs at about 1 iteration per second and there are 15918 words,
+so it'll take about 4.5 hours.
+
+[1]: https://www.powerlanguage.co.uk/wordle/
+[2]:
+  https://github.com/dwyl/english-words/blob/22d7c41119076750a96fca2acd664ed994cc0a75/words_alpha.txt
---
+++ src/main.rs
@@ -0,0 +1,62 @@
+use progressive::progress;
+
+type Word = [u8; 5];
+type Tiles = [Tile; 5];
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum Tile {
+  No,
+  Contains,
+  Yes,
+}
+
+fn get_tiles(guess: &Word, answer: &Word) -> Tiles {
+  let mut ret = [Tile::No; 5];
+  for (idx, tile) in ret.iter_mut().enumerate() {
+    let g = guess[idx];
+    if g == answer[idx] {
+      *tile = Tile::Yes;
+    } else if answer.contains(&g) {
+      *tile = Tile::Contains;
+    }
+  }
+  ret
+}
+
+fn might_be(guess: &Word, tiles: &Tiles, word: &Word) -> bool {
+  tiles.iter().enumerate().all(|(idx, tile)| {
+    let g = guess[idx];
+    match tile {
+      Tile::No => !word.contains(&g),
+      Tile::Contains => word.contains(&g),
+      Tile::Yes => word[idx] == g,
+    }
+  })
+}
+
+fn get_words() -> Vec<Word> {
+  include_str!("words_alpha.txt")
+    .lines()
+    .filter_map(|line| line.as_bytes().try_into().ok())
+    .collect()
+}
+
+fn main() {
+  let words = get_words();
+  let iter = words.iter().map(|guess| {
+    let total: usize = words
+      .iter()
+      .map(|answer| {
+        let tiles = get_tiles(guess, answer);
+        words
+          .iter()
+          .filter(|&word| might_be(guess, &tiles, word))
+          .count()
+      })
+      .sum();
+    (total, guess)
+  });
+  let (_, best) = progress(iter).min().unwrap();
+  let best = std::str::from_utf8(best).unwrap();
+  println!("best: {}", best);
+}
